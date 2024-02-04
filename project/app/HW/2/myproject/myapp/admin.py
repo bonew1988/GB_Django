5e92c2a9d37da8d6e4acbf7efc48202b7c06dae1
bonew1988 <bonew@bonew.ru>
2024-02-04 23:20:15 +0300

    add last HW_1

--- project/app/HW/2/myproject/myapp/admin.py
+++ project/app/HW/2/myproject/myapp/admin.py
@@ -1,9 +1,25 @@
 from django.contrib import admin
 from .models import Product, Client, Order
 
-admin.site.register(Product)
-admin.site.register(Client)
-admin.site.register(Order)
 
-# Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'phone_number',
+                    'address', 'registration_date')
+    search_fields = ('name', 'email', 'phone_number')
 
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'quantity', 'added_date')
+    search_fields = ('name', 'price', 'quantity')
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'client', 'total_amount', 'order_date')
+    search_fields = ('client__name', 'total_amount', 'order_date')
+
+    filter_horizontal = ('products',)
+
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Order, OrderAdmin)
